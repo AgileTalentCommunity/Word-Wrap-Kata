e21386944f35c74b7190224ed006cbe4d2f1db82
Sergio <sergio.villen.galera@hotmail.fr>
2015-12-13 18:19:48 +0100

    Should_return_word_newline_when_input_is_word_space_and_column_number_is_4 & Should_return_word_newline_word_when_input_is_wordword_and_column_number_is_4

--- WordWrap/WordWrap/WordWrapTests.cs
+++ WordWrap/WordWrap/WordWrapTests.cs
@@ -18,22 +18,51 @@ namespace WordWrap
                 WordWrap.Wrap(string.Empty, 0);
             });
         }
+
         [Test]
         public void Should_return_the_input_when_column_number_is_equals_or_bigger_than_input_lenght()
         {
             var actual = WordWrap.Wrap("Small", 10);
             Check.That(actual).IsEqualTo("Small");
         }
+
+
+
+        [Test]
+        public void Should_return_word_newline_when_input_is_word_space_and_column_number_is_4()
+        {
+            var actual = WordWrap.Wrap("word ", 4);
+            Check.That(actual).IsEqualTo("word\n");
+        }
+
+        [Test]
+        public void Should_return_word_newline_word_when_input_is_wordword_and_column_number_is_4()
+        {
+            var actual = WordWrap.Wrap("word word", 4);
+            Check.That(actual).IsEqualTo("word\nword");
+        }
     }
 
     internal class WordWrap
     {
-        internal static string Wrap(string input, int columNumber)
+        internal static string Wrap(string input, int columnNumber)
         {
-            if(columNumber < 1) throw new ArgumentOutOfRangeException(nameof(columNumber));
-            if (columNumber >= input.Length)
+            if(columnNumber < 1) throw new ArgumentOutOfRangeException(nameof(columnNumber));
+            var line = string.Empty;
+            if (columnNumber >= input.Length)
                 return input;
-            return input;
+            var inputArray = input.ToCharArray();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (i == columnNumber)
+                {
+                    if (inputArray[i] == ' ')
+                    {
+                        inputArray[i] = '\n';
+                    }
+                }
+            }
+            return new string(inputArray);            
         }
     }
 }

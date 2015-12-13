7dbe67ac0e0fef1996ba489f43889f1ddd3c2404
Sergio <sergio.villen.galera@hotmail.fr>
2015-12-13 20:10:05 +0100

    Refactoring

--- WordWrap/WordWrap/WordWrap.cs
+++ WordWrap/WordWrap/WordWrap.cs
@@ -9,39 +9,60 @@ namespace WordWrap
         internal static string Wrap(string input, int columnNumber)
         {
             if(columnNumber < 1) throw new ArgumentOutOfRangeException(nameof(columnNumber));
-            if (columnNumber >= input.Length)
-                return input;
+            if (columnNumber >= input.Length) return input;
             var inputArray = new List<char>();
             var inputTreated = string.Empty;
-            for (int i = 0; i < input.Length && i <= columnNumber; i++)
+            for (var i = 0; i < input.Length && i <= columnNumber; i++)
             {
                 inputArray.Add(input[i]);
-                if (i == columnNumber)
-                {
-                    if (input[i] == ' ')
+                if (!IsLastCharacterInColumn(columnNumber, i)) inputTreated += input[i];
+                else
+                {                    
+                    if (IsBreakOnWordAndHasAnSpace(input, i, inputArray))
                     {
-                        inputArray[i] = '\n';
-                        inputTreated += input[i];
-                    }
-                    else if (inputArray.Contains(' '))
-                    {
-                        var spacePosition = inputArray.FindLastIndex(c => c == ' ');
-                        inputArray[spacePosition] = '\n';
-                        inputArray = inputArray.Take(spacePosition + 1).ToList();
-                        inputTreated = input.Substring(0, spacePosition + 1);
+                        inputArray = ComeBackToLastSpace(input, inputArray, ref inputTreated);
                     }
                     else
                     {
-                        inputArray[i] = '\n';
+                        inputTreated = BreakOnWord(input, inputArray, i, inputTreated);
                     }
                 }
-                else
-                {
-                    inputTreated += input[i];
-                }
             }
-            //return input.ToString();
-            return new string(inputArray.ToArray()) + Wrap(input.Replace(inputTreated, ""), columnNumber);            
+            return CallRecursive(input, columnNumber, inputArray, inputTreated);            
+        }
+
+        private static string BreakOnWord(string input, List<char> inputArray, int i, string inputTreated)
+        {
+            inputArray[i] = '\n';
+            if (input[i] == ' ')
+            {
+                inputTreated += input[i];
+            }
+            return inputTreated;
+        }
+
+        private static bool IsBreakOnWordAndHasAnSpace(string input, int i, List<char> inputArray)
+        {
+            return input[i] != ' ' && inputArray.Contains(' ');
+        }
+
+        private static string CallRecursive(string input, int columnNumber, List<char> inputArray, string inputTreated)
+        {
+            return new string(inputArray.ToArray()) + Wrap(input.Replace(inputTreated, ""), columnNumber);
+        }
+
+        private static List<char> ComeBackToLastSpace(string input, List<char> inputArray, ref string inputTreated)
+        {
+            var spacePosition = inputArray.FindLastIndex(c => c == ' ');
+            inputArray[spacePosition] = '\n';
+            inputArray = inputArray.Take(spacePosition + 1).ToList();
+            inputTreated = input.Substring(0, spacePosition + 1);
+            return inputArray;
+        }
+
+        private static bool IsLastCharacterInColumn(int columnNumber, int i)
+        {
+            return i == columnNumber;
         }
     }
 }
